f9927484c1175d46683da06e878289fff22d2cf6
ROMANVIKI <vikiadhi2016@gmail.com>
2025-03-07 16:46:51 +0530

    db conf

--- dev/settings.py
+++ dev/settings.py
@@ -16,7 +16,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,11 +84,18 @@ WSGI_APPLICATION = "dev.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+# Replace the SQLite DATABASES configuration with PostgreSQL:
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"), conn_max_age=600
+    )
 }
 
 
